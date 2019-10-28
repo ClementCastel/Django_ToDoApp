d87aa25f60cb4536e1afd971ddd6a8e27e2c86be
Clément CASTEL <clement.castel75@gmail.com>
2019-10-28 15:42:10 +0100

    Day 2 : beginning

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -2,14 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class List (models.Model):
-	title = models.CharField(max_length=200)
-
-	def __str__ (self):
-		return self.title
-
 class Task (models.Model):
-	listP = models.ForeignKey(List, on_delete=models.CASCADE)
+	#listP = models.ForeignKey(List, on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
 	finishDate = models.DateTimeField('deadline date')
 	finished = models.BooleanField(default=False)
